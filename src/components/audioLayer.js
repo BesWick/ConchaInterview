4722ee8aa1d824d760cca2990939d849954fc2df
John Dobrota <wickthebest@gmail.com>
2021-01-13 19:59:43 -0800

    volume slider working

--- src/components/audioLayer.js
+++ src/components/audioLayer.js
@@ -19,32 +19,84 @@
 //Phase 1; get a audio player
 //Phase 2: pass audio to web audio
 import starwar from '../assets/starwars.wav'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Audio from './Audio'
+import './audioLayer.css'
+
 function AudioLayer() {
-    const [state, setState] = useState({
-        filter: 0,
-    })
+    // const audioContext = new (window.AudioContext ||
+    //     window.webkitAudioContext)()
+    // const gainNode = audioContext.createGain()
+
+    const audioElement = useRef()
+
+    // const track = autoctx.createMediaElementSource(audioElement)
+
+    //set state to represent initial value of masterGainNode
+    const [masterGainValue, setMasterGainValue] = useState(0)
+
+    const initializeMasterGain = () => {
+        const track = Audio.context.createMediaElementSource(
+            audioElement.current,
+        )
+        // Connect the masterGainNode to the audio context to allow it to output sound.
+        track.connect(Audio.masterGainNode)
+        Audio.masterGainNode.connect(Audio.context.destination)
+
+        // Set masterGain Value to 0
+        Audio.masterGainNode.gain.setValueAtTime(0, Audio.context.currentTime)
+    }
+
+    // const initializeMasterGain = () => {
+    //      track = audioContext.createMediaElementSource(
+    //         audioElement.current,
+    //     )
+    //     // Connect the masterGainNode to the audio context to allow it to output sound.
+    //     //  gainNode.connect(audiocontext.destination)
+    //     track.connect(gainNode).connect(audioContext.destination)
+
+    //     // Set masterGain Value to 0
+    //     gainNode.gain.setValueAtTime(0, audioContext.currentTime)
+    // }
+
+    //initialize masterGainNode on first render
+    useEffect(initializeMasterGain, [])
+
+    const changeMasterVolume = (e) => {
+        Audio.masterGainNode.gain.setValueAtTime(
+            e.target.value,
+            Audio.context.currentTime,
+        )
+        console.log(
+            'GAIN ' + Audio.masterGainNode.gain.value + ' ' + e.target.value,
+        )
+        setMasterGainValue(e.target.value)
+    }
 
     return (
         <div className='audio'>
             <div className='pl'>
-                <audio controls src={starwar}>
+                <audio controls src={starwar} ref={audioElement}>
                     Your browser does not support the
                     <code>audio</code> element.
                 </audio>
             </div>
-
-            <input
-                type='range'
-                min={-40}
-                max={40}
-                value={state.filter}
-                orient='vertical'
-                onChange={(e) =>
-                    setState({ ...state, filter: Number(e.target.value) })
-                }
-            />
-            <div className='num'>{state.filter}</div>
+            <div className='sliders'>
+                <div className='col'>
+                    <label htmlFor='fader'>&lt;1k</label>
+                    <input
+                        type='range'
+                        id='fader'
+                        min={0}
+                        max={1}
+                        step={0.01}
+                        value={masterGainValue}
+                        onChange={changeMasterVolume}
+                        orient='vertical'
+                    />
+                    <div className='num'>{masterGainValue}</div>
+                </div>
+            </div>
         </div>
     )
 }
